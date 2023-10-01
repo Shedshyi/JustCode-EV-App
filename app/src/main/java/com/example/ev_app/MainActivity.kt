113d5a703ef2734cd6fb71e77d3a9ea948542b5d
Shedshyu <90133181+NurikJK@users.noreply.github.com>
2023-10-01 13:08:37 +0600

    Layout is done

---
+++ app/src/main/java/com/example/ev_app/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.ev_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
